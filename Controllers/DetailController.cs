d8842a491189cab67e562185773725681e3fa138
Akash Kumar <akashkumar@whiteblue.in>
2022-12-19 12:36:03 +0530

    OrderMaster

---
+++ Controllers/DetailController.cs
@@ -0,0 +1,25 @@
+﻿using BikeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetailController : Controller
+    {
+        private readonly BikeContext _db;
+
+        public DetailController(BikeContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<OrderDetail>> Index()
+        {
+            List<OrderDetail> detail = new List<OrderDetail>();
+             detail = await _db.OrderDetails.Include(c => c.Product).Include(c => c.OrderMaster).ToListAsync();
+            return Ok(detail);
+        }
+    }
+}
---
+++ Controllers/MasterController.cs
@@ -0,0 +1,27 @@
+﻿using BikeApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasterController : ControllerBase
+    {
+        private readonly BikeContext _db;
+
+        public MasterController(BikeContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<OrderMaster>> Index()
+        {
+            List<OrderMaster> masters = new List<OrderMaster>();
+            masters = await _db.OrderMasters.ToListAsync();
+            return Ok(masters);
+        }
+    }
+}
+
